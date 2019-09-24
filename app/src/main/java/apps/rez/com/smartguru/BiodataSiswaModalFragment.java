7d1f470c88235855f3024ce6d5edbce4089b2a3a
Rez <moehamadrezha@gmail.com>
2019-09-25 01:19:04 +0700

    Perbaikan data yang dikirim ke fragment biodata_siswa

--- app/src/main/java/apps/rez/com/smartguru/BiodataSiswaModalFragment.java
+++ app/src/main/java/apps/rez/com/smartguru/BiodataSiswaModalFragment.java
@@ -15,6 +15,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,7 +35,7 @@ public class BiodataSiswaModalFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_modal_biodata_siswa, container, false);
         // Inflate the layout for this fragment
         getDialog().setTitle("Biodata");
-        getDialog().getWindow().setBackgroundDrawableResource(R.color.colorWhite);
+        Objects.requireNonNull(getDialog().getWindow()).setBackgroundDrawableResource(R.color.colorWhite);
 
         return view;
     }
@@ -43,5 +45,32 @@ public class BiodataSiswaModalFragment extends DialogFragment {
         super.onViewCreated(view, savedInstanceState);
 
         tvNama = view.findViewById(R.id.tvNamaSiswa);
+        tvNis = view.findViewById(R.id.tvNIS);
+        tvNisn = view.findViewById(R.id.tvNISN);
+        tvTempatLahir = view.findViewById(R.id.tvTempatLahir);
+        tvTanggalLahir = view.findViewById(R.id.tvTanggalLahir);
+        tvJenisKelamin = view.findViewById(R.id.tvJenisKelamin);
+        tvAgama = view.findViewById(R.id.tvAgama);
+        tvAlamat = view.findViewById(R.id.tvAlamat);
+        tvNamaAyah = view.findViewById(R.id.tvNamaAyah);
+        tvPekerjaanAyah = view.findViewById(R.id.tvPekerjaanAyah);
+        tvNamaIbu = view.findViewById(R.id.tvNamaIbu);
+        tvPekerjaanIbu = view.findViewById(R.id.tvPekerjaanIbu);
+        tvAlamatOrangTua = view.findViewById(R.id.tvAlamatOrangTua);
+
+        assert getArguments() != null;
+        tvNama.setText(getArguments().getString("NAMA_SISWA"));
+        tvNis.setText(getArguments().getString("NIS_SISWA"));
+        tvNisn.setText(getArguments().getString("NISN_SISWA"));
+        tvTempatLahir.setText(getArguments().getString("TEMPAT_LAHIR_SISWA"));
+        tvTanggalLahir.setText(getArguments().getString("TANGGAL_LAHIR_SISWA"));
+        tvJenisKelamin.setText(getArguments().getString("JENIS_KELAMIN_SISWA"));
+        tvAgama.setText(getArguments().getString("AGAMA_SISWA"));
+        tvAlamat.setText(getArguments().getString("ALAMAT_SISWA"));
+        tvNamaAyah.setText(getArguments().getString("NAMA_AYAH_SISWA"));
+        tvPekerjaanAyah.setText(getArguments().getString("PEKERJAAN_AYAH_SISWA"));
+        tvNamaIbu.setText(getArguments().getString("NAMA_IBU_SISWA"));
+        tvPekerjaanIbu.setText(getArguments().getString("PEKERJAAN_IBU_SISWA"));
+        tvAlamatOrangTua.setText(getArguments().getString("ALAMAT_AYAH_SISWA"));
     }
 }
---
+++ app/src/main/java/apps/rez/com/smartguru/Models/DataSiswa.java
@@ -0,0 +1,42 @@
+package apps.rez.com.smartguru.Models;
+
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.SerializedName;
+
+@Generated("com.robohorse.robopojogenerator")
+public class DataSiswa {
+
+    @SerializedName("data")
+    private List<DataSiswaItem> data;
+
+    @SerializedName("status")
+    private boolean status;
+
+    public void setData(List<DataSiswaItem> data) {
+        this.data = data;
+    }
+
+    public List<DataSiswaItem> getData() {
+        return data;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "DataSiswa{" +
+                        "data = '" + data + '\'' +
+                        ",status = '" + status + '\'' +
+                        "}";
+    }
+}
