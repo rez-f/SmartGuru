609b27f4e01dc115ad88619c63d433b7c4242495
Rez <moehamadrezha@gmail.com>
2019-10-15 12:45:01 +0700

    Perbaikan tampilan KelasBerkasFragment

--- app/src/main/java/apps/rez/com/smartguru/KelasBerkasFragment.java
+++ app/src/main/java/apps/rez/com/smartguru/KelasBerkasFragment.java
@@ -13,9 +13,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import apps.rez.com.smartguru.Rest.UtilsApi;
 
 
 /**
@@ -23,11 +29,12 @@ import java.io.File;
  */
 public class KelasBerkasFragment extends Fragment implements View.OnClickListener{
 
-    private String ID_KELAS;
     private int ID_MAPEL;
+    private String ID_KELAS;
+    private StringBuilder textUrl;
+    private boolean fileServerExist;
 
-    private Button transkrip;
-    private ProgressDialog loading;
+    private ImageView download;
 
     public KelasBerkasFragment() {
         // Required empty public constructor
@@ -39,11 +46,14 @@ public class KelasBerkasFragment extends Fragment implements View.OnClickListene
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_kelas_berkas, container, false);
 
-        transkrip = view.findViewById(R.id.btnTranskrip);
-        transkrip.setOnClickListener(this);
+        getServerIpFromFile();
+
+        download = view.findViewById(R.id.ivDownload);
+        download.setOnClickListener(this);
 
         ID_KELAS = getArguments().getString("ID_KELAS");
         ID_MAPEL = getArguments().getInt("ID_MAPEL");
+
 
         return view ;
     }
@@ -57,7 +67,7 @@ public class KelasBerkasFragment extends Fragment implements View.OnClickListene
     public void onClick(View view) {
 
         DownloadManager downloadmanager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse("http://192.168.100.14/k13-RestCI/api/nilai/transkrip?idKelas="+ ID_KELAS +"&idMapel="+ ID_MAPEL);
+        Uri uri = Uri.parse(textUrl+"/nilai/transkrip?idKelas="+ ID_KELAS +"&idMapel="+ ID_MAPEL);
 
         Toast.makeText(getActivity(), uri.toString(), Toast.LENGTH_LONG).show();
 
@@ -69,5 +79,28 @@ public class KelasBerkasFragment extends Fragment implements View.OnClickListene
         File file = new File(Environment.getExternalStorageDirectory(), "/smartguru/transkrip.pdf");
         request.setDestinationUri(Uri.fromFile(file));
         downloadmanager.enqueue(request);
+    }
+
+    private void getServerIpFromFile() {
+        File sdcard = Environment.getExternalStorageDirectory();
+
+        //Get the text file
+        File file = new File(sdcard, "server.txt");
+
+        textUrl = new StringBuilder();
+
+        //Read text from file
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                textUrl.append(line);
+            }
+            fileServerExist = true;
+            br.close();
+        } catch (IOException e) {
+            //You'll need to add proper error handling here
+            fileServerExist = false;
+        }
     }
 }
